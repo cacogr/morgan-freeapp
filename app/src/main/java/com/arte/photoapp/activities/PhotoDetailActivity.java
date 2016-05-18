8d89a137f2eba8f7fec2ee479a60f28a06c55822
cacogr <>
2016-05-18 21:40:26 +0100

    first commit

---
+++ app/src/main/java/com/arte/photoapp/activities/PhotoDetailActivity.java
@@ -0,0 +1,59 @@
+package com.arte.photoapp.activities;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.arte.photoapp.R;
+import com.arte.photoapp.fragments.PhotoDetailFragment;
+
+public class PhotoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+        setupActivity();
+        setupFragment(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpTo(this, new Intent(this, PhotoListActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupActivity() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupFragment(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        Bundle fragmentArguments = new Bundle();
+        fragmentArguments.putString(PhotoDetailFragment.ARG_PHOTO_ID, getIntent().getStringExtra(PhotoDetailFragment.ARG_PHOTO_ID));
+        PhotoDetailFragment fragment = new PhotoDetailFragment();
+        fragment.setArguments(fragmentArguments);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.photo_detail_container, fragment)
+                .commit();
+
+    }
+}
---
+++ app/src/main/java/com/arte/photoapp/model/Photo.java
@@ -0,0 +1,48 @@
+package com.arte.photoapp.model;
+
+
+import java.io.Serializable;
+
+public class Photo {
+
+    private String id;
+    private String title;
+    private String url;
+    private String thumbnailUrl;
+
+    public Photo() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return "https://placeholdit.imgix.net/~text?txtsize=14&bg=" + url.substring(url.length() - 6, url.length()) + "&txt=BIGGERWORLD&w=150&h=150";
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return "https://placeholdit.imgix.net/~text?txtsize=14&bg=" + thumbnailUrl.substring(thumbnailUrl.length() - 6, thumbnailUrl.length()) + "&txt=HELLOWORLD&w=150&h=150";
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
